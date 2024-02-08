f3f4c8cb0ba0d17b5d6f6e645b93f8f6bba0d149
Mansi Aggarwal <mansiaggarwal@cdacnoida.in>
2024-02-08 11:58:38 +0530

    Initial commit

---
+++ muhs-new - theme/js/slider/nivo-active.js
@@ -0,0 +1,18 @@
+(function ($) {
+"use strict";
+
+$('#mainSlider,.mainSlider').nivoSlider({
+	directionNav: true,
+	animSpeed: 500,
+	slices: 18,
+	pauseTime: 750000,
+	pauseOnHover: false,
+	controlNav: false,
+	prevText: '<i class="bi bi-arrow-left"></i>',
+	nextText: '<i class="bi bi-arrow-right"></i>'
+});
+
+
+
+
+})(jQuery);	
