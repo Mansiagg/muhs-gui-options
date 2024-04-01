21b7d4cc1a4f098f0b6b23d3cc3072f6d425455f
Mansi Aggarwal <mansiaggarwal@cdacnoida.in>
2024-04-01 17:30:38 +0530

    updates

---
+++ emms-up/assets/js/script.js
@@ -0,0 +1,17 @@
+$(document).ready(function() {
+
+
+    $("#services-slider").owlCarousel({
+        items : 4,
+        center:true,
+        itemsDesktop:[1199,3],
+        itemsDesktopSmall:[980,2],
+        itemsMobile : [600,1],
+        navigation:true,
+        navigationText:["",""],
+        pagination:true,
+        autoPlay:true
+    });
+
+   
+});
