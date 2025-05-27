8cdbd3b90d68673aa33698d6107ce4d04f83167c
Mansiagg <mansijims@gmail.com>
2025-05-27 18:09:47 +0530

    updates

---
+++ muhs-chakra/assets/js/script.js
@@ -0,0 +1,28 @@
+function hidePreloader() {
+  const preloader = document.getElementById('preloader');
+  const mainContent = document.getElementById('main-content');
+
+  preloader.style.display = 'none';
+  mainContent.style.display = 'block';
+  
+}
+// Automatically hide after 5 seconds as a fallback
+setTimeout(hidePreloader, 10000);
+
+
+const backToTopButton = document.getElementById('backToTop');
+
+window.addEventListener('scroll', () => {
+  if (window.scrollY > 300) {
+    backToTopButton.classList.add('show');
+  } else {
+    backToTopButton.classList.remove('show');
+  }
+});
+
+backToTopButton.addEventListener('click', () => {
+  window.scrollTo({
+    top: 0,
+    behavior: 'smooth'
+  });
+});
